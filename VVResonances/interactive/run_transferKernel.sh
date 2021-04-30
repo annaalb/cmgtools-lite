de1a70cda3b0d3d576fb7a953651f38a7488e6fb
IreneZoi <irene.zoi@cern.ch>
2021-04-30 14:15:10 +0200

    input dir as variable

--- VVResonances/interactive/run_transferKernel.sh
+++ VVResonances/interactive/run_transferKernel.sh
@@ -4,23 +4,24 @@ catIn=$1
 catOut=$2
 samples=(pythia herwig madgraph)
 labels=("" _altshapeUp _altshape2)
-period=$3 #Run2
-year=$4 #"2016,2017,2018"
+period=Run2
+year="2016,2017,2018"
+inputdir=results_Run2/
 #create the directory to store the results
 mkdir postfit_qcd
 
 #fit MC in catOut category with templates of catIn category
 for i in ${!samples[*]}
 do
-	python transferKernel.py -i results_${period}/JJ_${period}_nonRes_${catOut}${labels[$i]}.root --sample ${samples[$i]} --year ${year} -p xyz --pdfIn results_${period}/JJ_${period}_nonRes_3D_${catIn}.root --channel ${catOut}
+	python transferKernel.py -i ${inputdir}/JJ_${period}_nonRes_${catOut}${labels[$i]}.root --sample ${samples[$i]} --year ${year} -p xyz --pdfIn ${inputdir}/JJ_${period}_nonRes_3D_${catIn}.root --channel ${catOut}
 done
 
 #merge post-fit 1Dx2Dx2D templates for catOut category
-python transferKernel.py -i results_${period}/JJ_${period}_nonRes_${catOut}.root --year ${year} --pdfIn results_${period}/JJ_${period}_nonRes_3D_${catIn}.root --merge
+python transferKernel.py -i ${inputdir}/JJ_${period}_nonRes_${catOut}.root --year ${year} --pdfIn ${inputdir}/JJ_${period}_nonRes_3D_${catIn}.root --merge
 
 #make post-fit validation plots for category catOut
 for i in ${!samples[*]}
 do
-        python Projections3DHisto.py --mc results_${period}/JJ_${period}_nonRes_${catOut}${labels[$i]}.root,nonRes -k save_new_shapes_${period}_${samples[$i]}_${catOut}_3D.root,histo -o control-plots-${period}-${catOut}-${samples[$i]} --period "${period}"
-        python Projections3DHisto_HPHP.py --mc results_${period}/JJ_${period}_nonRes_${catOut}${labels[$i]}.root,nonRes -k save_new_shapes_${period}_${samples[$i]}_${catOut}_3D.root,histo -o control-plots-coarse-${period}-${catOut}-${samples[$i]} --period "${period}"
+        python Projections3DHisto.py --mc ${inputdir}/JJ_${period}_nonRes_${catOut}${labels[$i]}.root,nonRes -k save_new_shapes_${period}_${samples[$i]}_${catOut}_3D.root,histo -o control-plots-${period}-${catOut}-${samples[$i]} --period "${period}"
+        python Projections3DHisto_HPHP.py --mc ${inputdir}/JJ_${period}_nonRes_${catOut}${labels[$i]}.root,nonRes -k save_new_shapes_${period}_${samples[$i]}_${catOut}_3D.root,histo -o control-plots-coarse-${period}-${catOut}-${samples[$i]} --period "${period}"
 done	
